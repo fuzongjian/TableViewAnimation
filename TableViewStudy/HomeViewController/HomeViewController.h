3b96d1ee3c519ec90da7656f85297dd54db4bca5
付宗建 <fuzongjian@deltalphas-MacBookPro.local>
2016-08-14 17:23:25 +0800

    初始化

---
+++ TableViewStudy/HomeViewController/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  TableViewStudy
+//
+//  Created by 付宗建 on 16/8/14.
+//  Copyright © 2016年 youran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UITableViewController
+
+@end
---
+++ TableViewStudy/HomeViewController/SuperViewController.h
@@ -0,0 +1,13 @@
+//
+//  SuperViewController.h
+//  TableViewStudy
+//
+//  Created by 付宗建 on 16/8/14.
+//  Copyright © 2016年 youran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SuperViewController : UIViewController
+
+@end
