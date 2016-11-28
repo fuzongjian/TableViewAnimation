38219fbaef76fd3727b60682106d4d40be5fca59
付宗建 <fuzongjian@deltalphas-MacBookPro.local>
2016-11-28 16:47:27 +0800

    增加TableView和CollectionView联动

---
+++ TableViewStudy/CollectionViewLinkage/CollectionViewLinkage.h
@@ -0,0 +1,13 @@
+//
+//  CollectionViewLinkage.h
+//  TableViewStudy
+//
+//  Created by 付宗建 on 16/11/28.
+//  Copyright © 2016年 youran. All rights reserved.
+//
+
+#import "SuperViewController.h"
+
+@interface CollectionViewLinkage : SuperViewController
+
+@end
---
+++ TableViewStudy/TableViewLinkage/TableViewLinkage.m
@@ -0,0 +1,37 @@
+//
+//  TableViewLinkage.m
+//  TableViewStudy
+//
+//  Created by 付宗建 on 16/11/28.
+//  Copyright © 2016年 youran. All rights reserved.
+//
+
+#import "TableViewLinkage.h"
+
+@interface TableViewLinkage ()
+
+@end
+
+@implementation TableViewLinkage
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
